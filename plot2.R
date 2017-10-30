7b500adf7aff9a678f6111b9a01dee29b5923749
Graydon Snider <sniderg.mcgill@gmail.com>
2017-10-29 22:17:29 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,3 +1,13 @@
+#reading data file
+library(lubridate)
+library(dplyr)
+
+power <- read.csv("week1/household_power_consumption.txt",sep=";",na.strings = "?")
+power$Time <- dmy(power$Date) + hms(power$Time)
+
+power <- power %>% select(Time:Sub_metering_3) 
+
+power <- power[power_time >= date("2007-02-01") & power_time < date("2007-02-03"),]
 
 #part2
 png(filename="plot2.png")
